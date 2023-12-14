fa1639ef5298b7a846a039b31d298039f0804fc5
Tiago Sora <tiagogcarvalho@ua.pt>
2023-12-14 18:56:35 +0000

    issues solving

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,8 +4,17 @@ from api import main
 
 client = TestClient(main.app)
 
+urls = {
+    "base": "/points/v1",
+    "get_all_points": "/points/v1/points",
+    "get_point_by_name": "/points/v1/points/name",
+    "create_point": "/points/v1/points",
+    "get_point_id_of_access": "/points/v1/access",
+    "delete_point": "/points/v1/points/name",
+}
+
 def test_base():
-    response = client.get("/points/v1/")
+    response = client.get(urls["base"])
     assert response.status_code == 200
     assert response.json() == {"response": "Hello World!"}
 
@@ -17,7 +26,7 @@ def test_get_all_points(mock_get_points):
     ]
     mock_get_points.return_value = mock_points
     
-    response = client.get("/points/v1/points/")
+    response = client.get(urls["get_all_points"])
     assert response.status_code == 200
     assert response.json() == mock_points
 
@@ -26,12 +35,12 @@ def test_get_point_by_name(mock_get_point_by_name):
     mock_point = {"id": 1, "name": "point1", "location": "location1", "coordinates": "coordinates1", "image": "image1"}
     mock_get_point_by_name.return_value = mock_point
     
-    response = client.get("/points/v1/points/name/point1")
+    response = client.get(urls["get_point_by_name"]+"/point1")
     assert response.status_code == 200
     assert response.json() == mock_point
 
     mock_get_point_by_name.return_value = None
-    response = client.get("/points/v1/points/name/999")
+    response = client.get(urls["get_point_by_name"]+"/999")
     assert response.status_code == 204
 
 @patch("api.main.auth.verify_access")
@@ -43,12 +52,12 @@ def test_create_point(mock_create_point, mock_get_point_by_name, mock_verify_acc
     mock_point = {"id": 1, "name": "point1", "location": "location1", "coordinates": "coordinates1", "image": "image1"}
     mock_create_point.return_value = mock_point
     
-    response = client.post("/points/v1/points/", json = {"name": "point1", "location": "location1", "coordinates": "coordinates1", "image": "image1"})
+    response = client.post(urls["create_point"], json = {"name": "point1", "location": "location1", "coordinates": "coordinates1", "image": "image1"})
     assert response.status_code == 201
     assert response.json() == mock_point
 
     mock_get_point_by_name.return_value = mock_point
-    response = client.post("/points/v1/points/", json = {"name": "point1", "location": "location1", "coordinates": "coordinates1", "image": "image1"})
+    response = client.post(urls["create_point"], json = {"name": "point1", "location": "location1", "coordinates": "coordinates1", "image": "image1"})
     assert response.status_code == 409
     assert response.json() == {"detail": "POINT ALREADY REGISTERED"}
 
@@ -60,13 +69,13 @@ def test_get_point_id_of_access(mock_get_auth, mock_verify_access):
     mock_access = 1
     mock_get_auth.return_value = mock_access
     
-    response = client.get(f"/points/v1/access")
+    response = client.get(urls["get_point_id_of_access"])
     assert response.status_code == 200
     assert response.json() == mock_access
     
     mock_get_auth.return_value = None
 
-    response = client.get(f"/points/v1/access")
+    response = client.get(urls["get_point_id_of_access"])
     assert response.status_code == 204
 
 @patch("api.main.auth.verify_access")
@@ -78,10 +87,10 @@ def test_delete_point(mock_delete_point, mock_get_point_by_name, mock_verify_acc
     mock_get_point_by_name.return_value = mock_point
     mock_delete_point.return_value = "OK"
     
-    response = client.delete("/points/v1/points/name/point1")
+    response = client.delete(urls["delete_point"]+"/point1")
     assert response.status_code == 200
     assert response.json() == {"message": "POINT DELETED"}
 
     mock_delete_point.return_value = None
-    response = client.delete("/points/v1/points/name/point999")
+    response = client.delete(urls["delete_point"]+"/999")
     assert response.status_code == 204
